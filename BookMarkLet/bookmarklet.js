722a67665b4da9e250184c21b8edf04c1b06baf5
Abdullah Morrison <amorr128@uottawa.ca>
2019-11-23 14:34:56 -0500

    Add files via upload

---
+++ BookMarkLet/bookmarklet.js
@@ -0,0 +1,8 @@
+(function(){
+    console.log("bookmark starting");
+    let paragraphs = document.getElementsByTagName('p');
+    for(let i = 0; i<paragraphs.length; i++){
+        paragraphs[i].innerHTML = "ABDULLAH";
+    }
+})();
+
